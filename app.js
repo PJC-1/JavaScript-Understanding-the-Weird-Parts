7eaca16ab75cdc58e94c72701bdfc8cd9248cc03
PJC-1 <jumpzine@gmail.com>
2019-07-09 21:52:15 -0700

    added object reflection example

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var a = {};
-var b = function() { };
-var c = [];
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;
+    }
+}
 
-console.log(a.__proto__);
-console.log(b.__proto__);
-console.log(b.__proto__.__proto__);
-console.log(c.__proto__);
+var john = {
+    firstname: 'John',
+    lastname: 'Doe'
+}
+
+john.__proto__ = person;
+
+for (var prop in john) {
+    if (john.hasOwnProperty(prop)) {
+        console.log(prop + ': ' + john[prop]);
+    }
+}
+
+var jane = {
+    address: '111 Main St.',
+    getFormalFullName: function() {
+        return this.lastname + ', ' + this.firstname;
+    }
+}
+
+var jim = {
+    getFirstName: function() {
+        return firstname;
+    }
+}
+
+_.extend(john, jane, jim);
+
+console.log(john);
