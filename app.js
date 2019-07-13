177236e5df78a7039e6b72a8eb7d1da47c57d595
PJC-1 <jumpzine@gmail.com>
2019-07-12 22:57:01 -0700

    added polyfil example with Object.create

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-String.prototype.isLengthGreatThan = function(limit) {
-    return this.length > limit;
+if (!Object.create) {
+    Object.create = function (o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation' + ' only accepts the first parameter.');
+        }
+        function F() {}
+        F.prototype = o;
+        return new F();
+    };
 }
 
-console.log("John".isLengthGreatThan(3));
-
-Number.prototype.isPositive = function() {
-    return this > 0;
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstname;
+    }
 }
 
-var a = new Number(3);
-console.log(a.isPositive());
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
+console.log(john);
