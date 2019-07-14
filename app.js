bcbef33f8d61fec6b0e1abdf7d5ab34100cc9081
PJC-1 <jumpzine@gmail.com>
2019-07-13 23:36:38 -0700

    adding es6 class and extends example

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-if (!Object.create) {
-    Object.create = function (o) {
-        if (arguments.length > 1) {
-            throw new Error('Object.create implementation' + ' only accepts the first parameter.');
-        }
-        function F() {}
-        F.prototype = o;
-        return new F();
-    };
-}
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 
-var person = {
-    firstname: 'Default',
-    lastname: 'Default',
-    greet: function() {
-        return 'Hi ' + this.firstname;
+    greet() {
+        return 'Hi ' + firstname;
     }
 }
 
-var john = Object.create(person);
-john.firstname = 'John';
-john.lastname = 'Doe';
-console.log(john);
+var john = new Person('John', 'Doe');
+
+
+class InformalPerson extends Person {
+    constructor(firstname, lastname) {
+        super(firstname, lastname);
+    }
+
+    greet() {
+        return 'Yo ' + firstname;
+    }
+}
